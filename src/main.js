f87ad83f10a911aa30199ab2b4cba21441c1b48b
arelia10 <arelia101195@gmail.com>
2022-03-07 22:58:51 -0500

    cambios css y html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import App from './components/App.js';
 import pokemon from './data/pokemon/pokemon.js';
 import webdev from './data/webdev/webdev.js';
-const  image=document.getElementById('image');
+/*const  image=document.getElementById('image');
 image.innerHTML=`${pokemon.items}`
 console.log(image.innerHTML=`${pokemon.items}`)
-/*document.getElementById('root').appendChild(App());*/
+document.getElementById('root').appendChild(App());*/
+const start = document.getElementById("go"); 
+
+start.addEventListener("click", () => {
+  
+  const windowinitial = document.querySelector(".windowinitial");
+ const window= document.querySelector(".window");
+  windowinitial.style.display = "none";
+  window.style.display = "block";
+
+});
